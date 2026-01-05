a635426514d0062d4dd57caff7e67fd67b6ad168
rafidoth <rafiulhasan803@gmail.com>
2026-01-05 14:00:32 +0600

    landing page removed

--- landing/app/sections/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as Header } from "./Header";
-export { default as Hero } from "./Hero";
-export { default as LogosStrip } from "./LogosStrip";
-export { default as Features } from "./Features";
-export { default as HowItWorks } from "./HowItWorks";
-export { default as Pricing } from "./Pricing";
-export { default as FAQ } from "./FAQ";
-export { default as Footer } from "./Footer";
