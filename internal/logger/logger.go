ec13aa20d4dabc59f6988b1335f4a3f54da5ee29
rafidoth <rafiulhasan803@gmail.com>
2026-03-14 17:26:31 +0600

    zerolog setup with new relic integration

---
+++ internal/logger/logger.go
@@ -0,0 +1,54 @@
+package logger
+
+import (
+	"os"
+	"time"
+
+	"github.com/newrelic/go-agent/v3/newrelic"
+	"github.com/rafidoth/onlyexams/internal/config"
+)
+
+type LoggerService struct {
+	nrApp *newrelic.Application
+}
+
+func NewLoggerService(cfg *config.ObservabilityConfig) *LoggerService {
+	lService := &LoggerService{}
+
+	if cfg.NewRelic.LicenseKey == "" {
+		return lService
+	}
+
+	var configOptions []newrelic.ConfigOption
+	configOptions = append(configOptions,
+		newrelic.ConfigAppName(cfg.ServiceName),
+		newrelic.ConfigLicense(cfg.NewRelic.LicenseKey),
+		newrelic.ConfigAppLogForwardingEnabled(cfg.NewRelic.AppLogForwardingEnabled),
+		newrelic.ConfigDistributedTracerEnabled(cfg.NewRelic.DistributedTracingEnabled),
+	)
+
+	// Add debug logging only if explicitly enabled
+	if cfg.NewRelic.DebugLogging {
+		configOptions = append(configOptions, newrelic.ConfigDebugLogger(os.Stdout))
+	}
+
+	app, err := newrelic.NewApplication(configOptions...)
+	if err != nil {
+		return lService
+	}
+
+	lService.nrApp = app
+	return lService
+}
+
+// Shutdown shuts down New Relic
+func (ls *LoggerService) Shutdown() {
+	if ls.nrApp != nil {
+		ls.nrApp.Shutdown(10 * time.Second)
+	}
+}
+
+// GetApplication returns the New Relic application instance
+func (ls *LoggerService) GetApplication() *newrelic.Application {
+	return ls.nrApp
+}
