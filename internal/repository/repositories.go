db9a18c53925fe1ea7a1997045a2949c1661e7bc
rafidoth <rafiulhasan803@gmail.com>
2026-03-15 13:32:53 +0600

    add repository pattern

---
+++ internal/repository/repositories.go
@@ -0,0 +1,19 @@
+package repository
+
+import "github.com/rafidoth/onlyexams/internal/server"
+
+type Repositories struct {
+	User     *UserRepository
+	Set      *SetRepository
+	Question *QuestionRepository
+	Exam     *ExamRepository
+}
+
+func NewRepositories(s *server.Server) *Repositories {
+	return &Repositories{
+		User:     NewUserRepository(s),
+		Set:      NewSetRepository(s),
+		Question: NewQuestionRepository(s),
+		Exam:     NewExamRepository(s),
+	}
+}
