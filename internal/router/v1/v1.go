efbe8e51222a0cc5925bd58e0c7b0032177007e8
rafidoth <rafiulhasan803@gmail.com>
2026-03-16 00:20:52 +0600

    add v1 api , orchestration of all routes

---
+++ internal/router/v1/v1.go
@@ -0,0 +1,15 @@
+package v1
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/rafidoth/onlyexams/internal/handler"
+)
+
+// RegisterV1Routes mounts all v1 API sub-routes onto the given chi.Router.
+func RegisterV1Routes(r chi.Router, h *handler.Handlers) {
+	registerUserRoutes(r, h.User)
+	registerSetRoutes(r, h.Set)
+	registerQuestionRoutes(r, h.Question)
+	registerExamRoutes(r, h.Exam)
+	registerSubmissionRoutes(r, h.Submission)
+}
