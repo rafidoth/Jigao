94882a95ebf691a6d355b2b206d8f0fdb2e3386f
rafidoth <rafiulhasan803@gmail.com>
2026-03-16 00:20:29 +0600

    add sets route

---
+++ internal/router/v1/sets.go
@@ -0,0 +1,20 @@
+package v1
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/rafidoth/onlyexams/internal/handler"
+)
+
+func registerSetRoutes(r chi.Router, h *handler.SetHandler) {
+	r.Route("/sets", func(r chi.Router) {
+		r.Get("/", h.GetRecentSets)
+		r.Post("/", h.CreateNewSet)
+		r.Post("/gen", h.GenerateQuestionSet)
+		r.Post("/save_generated", h.SaveGeneratedQuestions)
+		r.Get("/{set_id}", h.GetASet)
+		r.Put("/{set_id}", h.UpdateASet)
+		r.Delete("/{set_id}", h.DeleteASet)
+		r.Get("/access_list/{set_id}", h.GetSetAccessList)
+		r.Post("/access", h.AllowSetAccess)
+	})
+}
