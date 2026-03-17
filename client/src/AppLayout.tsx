e1270481bd05e87feb52d43359dbfd0e2e0b09b1
rafidoth <rafiulhasan803@gmail.com>
2026-03-17 16:02:06 +0600

    ui: thin sidebar improvement

--- client/src/AppLayout.tsx
+++ client/src/AppLayout.tsx
@@ -7,54 +7,54 @@ import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
 
 function AppLayout() {
-  const show = useSidebarStore((state) => state.show);
-  const hamburgerMenu = useSidebarStore((state) => state.hamburgerMenu);
-  const toggleHamburgerMenu = useSidebarStore(
-    (state) => state.toggleHamburgerMenu,
-  );
+    const show = useSidebarStore((state) => state.show);
+    const hamburgerMenu = useSidebarStore((state) => state.hamburgerMenu);
+    const toggleHamburgerMenu = useSidebarStore(
+        (state) => state.toggleHamburgerMenu,
+    );
 
-  return (
-    <div className="flex h-screen w-screen overflow-hidden">
-      {show ? (
-        <div className="hidden md:block w-[15%] h-full border-r">
-          <Sidebar />
-        </div>
-      ) : (
-        <div className="hidden md:block w-[3%] h-full border-none">
-          <ThinSidebar />
-        </div>
-      )}
+    return (
+        <div className="flex h-screen w-screen overflow-hidden">
+            {show ? (
+                <div className="hidden md:block w-[15%] h-full border-r">
+                    <Sidebar />
+                </div>
+            ) : (
+                <div className="hidden md:block w-[80px] h-full border-none">
+                    <ThinSidebar />
+                </div>
+            )}
 
-      {hamburgerMenu && (
-        <div className="fixed inset-0 z-50 bg-background">
-          <FullScreenSidebar />
-        </div>
-      )}
+            {hamburgerMenu && (
+                <div className="fixed inset-0 z-50 bg-background">
+                    <FullScreenSidebar />
+                </div>
+            )}
 
-      {/* Main content */}
-      {!hamburgerMenu && (
-        <div
-          className={`relative h-full flex-1 overflow-hidden ${show ? "md:w-[85%]" : "md:w-[95%]"}`}
-        >
-          <main className="h-full w-full">
-            {/* Mobile hamburger button */}
-            <div className="fixed bottom-8 right-8 z-50 lg:hidden">
-              <Button
-                variant="outline"
-                size="icon"
-                className="rounded-full size-12"
-                onClick={() => toggleHamburgerMenu()}
-                aria-label="Toggle sidebar"
-              >
-                <Menu className="h-6 w-6" />
-              </Button>
-            </div>
-            <Outlet />
-          </main>
+            {/* Main content */}
+            {!hamburgerMenu && (
+                <div
+                    className={`relative h-full flex-1 overflow-hidden ${show ? "md:w-[85%]" : "md:w-[95%]"}`}
+                >
+                    <main className="h-full w-full">
+                        {/* Mobile hamburger button */}
+                        <div className="fixed bottom-8 right-8 z-50 lg:hidden">
+                            <Button
+                                variant="outline"
+                                size="icon"
+                                className="rounded-full size-12"
+                                onClick={() => toggleHamburgerMenu()}
+                                aria-label="Toggle sidebar"
+                            >
+                                <Menu className="h-6 w-6" />
+                            </Button>
+                        </div>
+                        <Outlet />
+                    </main>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default AppLayout;
--- client/src/components/thin_sidebar.tsx
+++ client/src/components/thin_sidebar.tsx
@@ -1,155 +1,134 @@
 import React from "react";
 import {
-  Wand2 as MagicWandIcon,
-  Archive as ArchiveIcon,
-  Backpack as BackpackIcon,
-  Sun,
-  Moon,
-  PanelRightClose as ViewVerticalIcon,
-  LogOut,
-  ClipboardList,
+    Wand2 as MagicWandIcon,
+    Archive as ArchiveIcon,
+    Backpack as BackpackIcon,
+    LogOut,
 } from "lucide-react";
 import { NavLink, useLocation, matchPath } from "react-router";
-import useSidebarStore from "../store/sidebarStore";
-import useThemeStore from "../store/themeStore";
+import useAuthStore from "@/store/authStore";
 import { Button } from "./ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import useAuthStore from "@/store/authStore";
 import { Popover, PopoverTrigger, PopoverContent } from "./ui/popover";
+import { cn } from "@/lib/utils";
 
 interface NavItem {
-  to: string;
-  icon: React.ReactNode;
-  activeIcon: React.ReactNode;
-  label: string;
+    to: string;
+    icon: React.ReactNode;
+    activeIcon: React.ReactNode;
+    label: string;
 }
 
 function ThinSidebar() {
-  const location = useLocation();
-  const navItems: NavItem[] = [
-    {
-      to: "/",
-      icon: <MagicWandIcon className="h-6 w-6" />,
-      activeIcon: <MagicWandIcon fill="white" className="h-6 w-6" />,
-      label: "Create Questions",
-    },
-    {
-      to: "/sets/",
-      icon: <ArchiveIcon className="h-6 w-6 " />,
-      activeIcon: <ArchiveIcon fill="white" className="h-6 w-6 text-white" />,
-      label: "My Sets",
-    },
-    {
-      to: "/exams",
-      icon: <BackpackIcon className="h-6 w-6" />,
-      activeIcon: <BackpackIcon fill="white" className="h-6 w-6 text-white" />,
-      label: "Exams",
-    },
-    {
-      to: "/question-bank",
-      icon: <ClipboardList className="h-6 w-6" />,
-      activeIcon: <ClipboardList fill="white" className="h-6 w-6 text-white" />,
-      label: "Question Bank",
-    },
-  ];
+    const location = useLocation();
+    const sizeClass = "h-4 w-4"
+    const navItems: NavItem[] = [
+        {
+            to: "/",
+            icon: <MagicWandIcon className={cn(sizeClass)} />,
+            activeIcon: <MagicWandIcon fill="white" className={cn(sizeClass)} />,
+            label: "Create Questions",
+        },
+        {
+            to: "/sets/",
+            icon: <ArchiveIcon className={cn(sizeClass)} />,
+            activeIcon: <ArchiveIcon fill="white" className={cn(sizeClass)} />,
+            label: "My Sets",
+        },
+        {
+            to: "/exams",
+            icon: <BackpackIcon className={cn(sizeClass)} />,
+            activeIcon: <BackpackIcon fill="white" className={cn(sizeClass)} />,
+            label: "Exams",
+        }
+    ];
 
-  const theme = useThemeStore((state) => state.theme);
-  const toggleTheme = useThemeStore((state) => state.toggleTheme);
-  const toggleSidebar = useSidebarStore((state) => state.toggleSidebar);
+    const isActive = (to: string) =>
+        matchPath({ path: to, end: true }, location.pathname) !== null;
 
-  const themeIcon =
-    theme === "light" ? (
-      <Moon className="h-6 w-6 text-muted-foreground" />
-    ) : (
-      <Sun className="h-6 w-6 text-muted-foreground" />
-    );
-
-  const isActive = (to: string) =>
-    matchPath({ path: to, end: true }, location.pathname) !== null;
-
-  return (
-    <div className="flex-col w-full h-full  hidden lg:flex bg-sidebar text-sidebar-foreground justify-between">
-      <div>
-        <div className="flex flex-col items-center gap-3">
-          <span className="text-2xl font-bold">
-            <img
-              src={"/logo2.png"}
-              alt="Jigao"
-              className="scale-60  mt-2 rounded-xl"
-            />
-          </span>
-        </div>
-        <div
-          className="flex flex-col gap-3 mt-6 w-full"
-          role="navigation"
-          aria-label="Secondary"
-        >
-          {navItems.map((item) => {
-            const active = isActive(item.to);
-            return (
-              <NavLink
-                key={item.to}
-                to={item.to}
-                style={{
-                  display: "block",
-                  textDecoration: "none",
-                  width: "100%",
-                }}
-              >
-                <div
-                  className={`flex justify-center items-center}`}
-                  style={{
-                    padding: "10px 5px",
-                    width: "100%",
-                    height: "100%",
-                    borderRadius: 15,
-                  }}
-                >
-                  <span className="sr-only">{item.label}</span>
-                  {active ? item.activeIcon : item.icon}
+    return (
+        <div className="flex-col w-full h-full  hidden lg:flex bg-sidebar text-sidebar-foreground justify-between">
+            <div>
+                <div className="flex flex-col items-center gap-3">
+                    <span className="text-2xl font-bold">
+                        <img
+                            src={"/logo2.png"}
+                            alt="Jigao"
+                            className="scale-60  mt-2 rounded-xl"
+                        />
+                    </span>
                 </div>
-              </NavLink>
-            );
-          })}
+                <div
+                    className="flex flex-col gap-3 mt-6 w-full"
+                    role="navigation"
+                    aria-label="Secondary"
+                >
+                    {navItems.map((item) => {
+                        const active = isActive(item.to);
+                        return (
+                            <NavLink
+                                key={item.to}
+                                to={item.to}
+                                style={{
+                                    display: "block",
+                                    textDecoration: "none",
+                                    width: "100%",
+                                }}
+                            >
+                                <div
+                                    className={`flex justify-center items-center}`}
+                                    style={{
+                                        padding: "10px 5px",
+                                        width: "100%",
+                                        height: "100%",
+                                        borderRadius: 15,
+                                    }}
+                                >
+                                    <span className="sr-only">{item.label}</span>
+                                    {active ? item.activeIcon : item.icon}
+                                </div>
+                            </NavLink>
+                        );
+                    })}
+                </div>
+            </div>
+            <div className="flex flex-col gap-y-2 items-center">
+                <LogoutButton />
+            </div>
         </div>
-      </div>
-      <div className="flex flex-col gap-y-2 items-center">
-        <LogoutButton />
-      </div>
-    </div>
-  );
+    );
 }
 
 const LogoutButton = () => {
-  const clerkFns = useAuthStore((state) => state.clerkFns);
-  const currentUserDetails = useAuthStore((state) => state.currentUserDetails);
+    const clerkFns = useAuthStore((state) => state.clerkFns);
+    const currentUserDetails = useAuthStore((state) => state.currentUserDetails);
 
-  return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Avatar className="w-10 h-10 cursor-pointer">
-          <AvatarImage src={currentUserDetails?.imageUrl} />
-          <AvatarFallback>
-            {currentUserDetails?.fullName?.substring(0, 2)}
-          </AvatarFallback>
-        </Avatar>
-      </PopoverTrigger>
-      <PopoverContent className="w-fit p-2" align="center" side="top">
-        <Button
-          variant="ghost"
-          onClick={async () => {
-            try {
-              await clerkFns?.signOut();
-            } catch (error) {
-              console.error(error);
-            }
-          }}
-        >
-          <LogOut className="mr-2 h-4 w-4" /> Logout
-        </Button>
-      </PopoverContent>
-    </Popover>
-  );
+    return (
+        <Popover>
+            <PopoverTrigger asChild>
+                <Avatar className="w-10 h-10 cursor-pointer">
+                    <AvatarImage src={currentUserDetails?.imageUrl} />
+                    <AvatarFallback>
+                        {currentUserDetails?.fullName?.substring(0, 2)}
+                    </AvatarFallback>
+                </Avatar>
+            </PopoverTrigger>
+            <PopoverContent className="w-fit p-2" align="center" side="top">
+                <Button
+                    variant="ghost"
+                    onClick={async () => {
+                        try {
+                            await clerkFns?.signOut();
+                        } catch (error) {
+                            console.error(error);
+                        }
+                    }}
+                >
+                    <LogOut className="mr-2 h-4 w-4" /> Logout
+                </Button>
+            </PopoverContent>
+        </Popover>
+    );
 };
 
 export default ThinSidebar;
