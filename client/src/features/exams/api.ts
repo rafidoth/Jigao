c3556f5b5671f66fc89170d12390e243f188cbce
rafidoth <rafiulhasan803@gmail.com>
2026-03-21 05:43:27 +0600

    chore(client): remove obsolete feature-local api helpers

--- client/src/features/exams/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-import type { Exam } from "./types";
-
-export async function getExams(): Promise<Exam[]> {
-  const res = await axios.get(`/api/v1/exams`);
-  return res.data as Exam[];
-}
